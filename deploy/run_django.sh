6aedbe45fb35d4a851bc2f3d3242c66adf4a8614
S3raphimCS <700520serj@mail.ru>
2025-11-15 18:35:30 +1000

    [fix] fix deploy

--- deploy/run_django.sh
+++ deploy/run_django.sh
@@ -4,6 +4,6 @@ python "manage.py" migrate --noinput
 
 python "manage.py" collectstatic --noinput
 
-python "manage.py" loaddata fixtures/example_data.json
+python "manage.py" loaddata server/fixtures/example_data.json
 
 gunicorn -c "$PROJECT_ROOT/gunicorn.conf.py" server.wsgi:application
